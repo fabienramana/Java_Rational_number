fc1f77625f5a67c2c1c911ef92f3b3168c157095
fabienramana <fabien.rmnd@gmail.com>
2019-04-08 17:57:35 +0200

    step 1.1

--- main.java
+++ main.java
@@ -1,6 +1,39 @@
 class main{
 
     public static void main(String[] args){
-        System.out.println("0");
+        int argc = args.length;
+        if(argc == 4){
+            int n1 = 0;
+            int d1 = 0;
+            int n2 = 0;
+            int d2 = 0;
+
+            try{
+                int numerator1;
+                int numerator2;
+                int denominator;
+
+                n1 = Integer.parseInt(args[0]);
+                d1 = Integer.parseInt(args[1]);
+                n2 = Integer.parseInt(args[2]);
+                d2 = Integer.parseInt(args[3]);
+
+                numerator1 = n1*d2;
+                numerator2 = d1*n2;
+                denominator = d1*d2;
+
+                StringBuilder s = new StringBuilder();
+                s.append(numerator1 + numerator2);
+                s.append("/");
+                s.append(denominator);
+                System.out.println(s);
+
+            }
+            catch(NumberFormatException e){
+                System.out.println("Les arguments entrés ne sont pas valides !");
+            }
+        }else{
+            System.out.println("Le nombre d'arguments est incorrect");
+        }
     }
 }
