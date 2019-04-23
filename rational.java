d40d8476ed0a68471904e9ef34c7671ba71944da
fabienramana <fabien.rmnd@gmail.com>
2019-04-23 09:52:04 +0200

    add step 1.19.6

--- rational.java
+++ rational.java
@@ -13,6 +13,29 @@ class rational{
         return toReturn;
     }
 
+    public static rational difference(rational r1, rational r2){
+        rational toReturn = new rational((r1.numerator*r2.denominator)-(r2.numerator*r1.denominator), r1.denominator*r2.denominator);
+        return toReturn;
+    }
+
+    public static int calculatePGCD(int a, int b){
+        int reste = 0;
+        reste = a % b;
+        if(reste != 0){
+        return calculatePGCD(b,reste);
+        }
+        else{
+            return java.lang.Math.abs(b);
+        }
+    }
+
+    public static rational divideByPGCD(rational r3, int pgcd){
+        r3.numerator = r3.numerator/ pgcd;
+        r3.denominator = r3.denominator/ pgcd;
+        return r3;
+    }
+
+
     /*@Override
     String toString(rational r3){
         System.out.println(r3.numerator + "/" + r3.denominator);
@@ -27,6 +50,7 @@ class rational{
         int d1 = 0;
         int n2 = 0;
         int d2 = 0;
+        int pgcd = 0;
 
         try{
 
@@ -43,7 +67,15 @@ class rational{
             rational r2 = new rational(n2, d2);
             rational r3 = new rational(0, 0);
 
-            r3 = rational.add(r1, r2);
+            if(operator == "-"){
+                r3 = rational.difference(r1, r2);
+            }
+            else if(operator == "+"){
+                r3 = rational.add(r1, r2);
+            }
+
+            pgcd = calculatePGCD(r3.numerator, r3.denominator);
+            r3 = divideByPGCD(r3, pgcd);
             rational.show(r3);
 
         }
